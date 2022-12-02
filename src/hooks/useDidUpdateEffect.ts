ea8c7466475091e5fa0045df0a1cb07dc5c66ee5
iadokonat <iadokonat0106@gmail.com>
2022-12-02 23:28:19 +0900

    fix

---
+++ src/hooks/useDidUpdateEffect.ts
@@ -0,0 +1,14 @@
+import { DependencyList, EffectCallback, useEffect, useRef } from 'react'
+
+export const useDidUpdateEffect = (fn: EffectCallback, deps: DependencyList) => {
+  const didMountRef = useRef(false)
+
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true
+    } else {
+      fn()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps)
+}
