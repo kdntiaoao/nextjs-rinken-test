f7bcc584f44602b11ea6dff015f5a55d78c9c6ec
iadokonat <iadokonat0106@gmail.com>
2022-12-23 11:07:11 +0900

    fix history page

--- src/components/organisms/presentations/LearningRecord/index.tsx
+++ src/components/organisms/presentations/LearningRecord/index.tsx
@@ -51,7 +51,8 @@ export const LearningRecord = memo(({ calendarArray, learningDays, today }: Prop
                     <td
                       key={j.toString()}
                       className={`h-10 rounded text-sm ${
-                        Number(format(day, 'd')) === Number(format(today, 'd')) && 'border-2 border-secondary-500'
+                        Number(format(day, 'd')) === Number(format(today, 'd')) &&
+                        'border-2 border-secondary-500 font-bold'
                       } ${getDayBgcolor(learningDays[Number(format(day, 'd')) - 1])}`}
                       aria-label={`${format(day, 'M月d日')}は${
                         learningDays[Number(format(day, 'd')) - 1]
