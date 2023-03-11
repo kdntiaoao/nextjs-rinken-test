4e2d677fe67d0e8040acfb38b6612fe8001acefc
iadokonat <iadokonat0106@gmail.com>
2023-03-11 16:58:16 +0900

    Show logout confirmation dialog

--- src/components/molecules/HeaderNav/index.tsx
+++ src/components/molecules/HeaderNav/index.tsx
@@ -4,7 +4,7 @@ import { memo } from 'react'
 import { Badge, HeaderNavButton } from 'components/atoms'
 
 type Props = {
-  menus: { title: string; url: string; badge?: number; onClick?: () => void }[]
+  menus: { title: string; url?: string; badge?: number; isLink?: boolean; onClick?: () => void }[]
 }
 
 // eslint-disable-next-line react/display-name
@@ -12,12 +12,23 @@ export const HeaderNav = memo(({ menus }: Props) => {
   return (
     <nav>
       <ul>
-        {menus.map(({ title, url, badge, onClick }, index) => (
+        {menus.map(({ title, url, badge, isLink = true, onClick }, index) => (
           <li
             key={title}
             className={`${index !== 0 && 'border-t'} select-none border-t-primary-100 dark:border-t-slate-600`}
           >
-            <Link href={url}>
+            {isLink && url !== undefined ? (
+              <Link href={url}>
+                <HeaderNavButton onClick={onClick}>
+                  {title}
+                  {badge ? (
+                    <Badge color="warning" aria-label={`見直しの問題が${badge}問あります。`}>
+                      {badge}
+                    </Badge>
+                  ) : null}
+                </HeaderNavButton>
+              </Link>
+            ) : (
               <HeaderNavButton onClick={onClick}>
                 {title}
                 {badge ? (
@@ -26,7 +37,7 @@ export const HeaderNav = memo(({ menus }: Props) => {
                   </Badge>
                 ) : null}
               </HeaderNavButton>
-            </Link>
+            )}
           </li>
         ))}
       </ul>
--- src/components/organisms/containers/HeaderContainer/index.tsx
+++ src/components/organisms/containers/HeaderContainer/index.tsx
@@ -1,5 +1,7 @@
+import { useRouter } from 'next/router'
 import { memo, MouseEvent, useCallback, useMemo, useState } from 'react'
 
+import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline'
 import { signOut } from 'firebase/auth'
 import { useAtom, useAtomValue } from 'jotai'
 import { useUpdateAtom } from 'jotai/utils'
@@ -10,22 +12,31 @@ import { auth } from '../../../../../firebase/client'
 import { authUserAtom } from 'atoms/authUserAtom'
 import { darkModeAtom } from 'atoms/darkModeAtom'
 import { incorrectsAtom } from 'atoms/incorrectsAtom'
+import { PrimaryButton } from 'components/atoms'
+import { AlertDialog } from 'components/molecules'
 import { Header } from 'components/organisms/presentations/Header'
 
 const preventPropagation = (event: MouseEvent<HTMLElement>) => event.stopPropagation()
 
 // eslint-disable-next-line react/display-name
 export const HeaderContainer = memo(() => {
+  const router = useRouter()
   const { resolvedTheme, setTheme } = useTheme()
   const [openMenu, setOpenMenu] = useState<boolean>(false)
+  const [openLogoutDialog, setOpenLogoutDialog] = useState<boolean>(false)
   const [authUser, setAuthUser] = useAtom(authUserAtom)
   const incorrects = useAtomValue(incorrectsAtom)
   const setDarkMode = useUpdateAtom(darkModeAtom)
 
+  const handleToggleLogoutDialog = useCallback(() => {
+    setOpenLogoutDialog((prev) => !prev)
+  }, [])
+
   const handleLogout = useCallback(async () => {
     await signOut(auth)
     setAuthUser(null)
-  }, [setAuthUser])
+    router.push('accounts//login')
+  }, [router, setAuthUser])
 
   const changeTheme = useCallback(() => {
     setDarkMode(resolvedTheme !== 'dark')
@@ -43,23 +54,46 @@ export const HeaderContainer = memo(() => {
     if (authUser?.uid) {
       array.push(
         { title: '履歴', url: '/history' },
-        { title: 'ログアウト', url: '/accounts/login', onClick: handleLogout }
+        { title: 'ログアウト', isLink: false, onClick: handleToggleLogoutDialog }
       )
     } else {
       array.push({ title: 'ログイン', url: '/accounts/login' })
     }
     return array
-  }, [authUser?.uid, handleLogout, incorrects?.length])
+  }, [authUser?.uid, handleToggleLogoutDialog, incorrects?.length])
 
   return (
-    <Header
-      authUser={authUser}
-      darkMode={resolvedTheme === 'dark'}
-      menus={menus}
-      openMenu={openMenu}
-      changeTheme={changeTheme}
-      handleToggleMenu={handleToggleMenu}
-      preventPropagation={preventPropagation}
-    />
+    <>
+      <Header
+        authUser={authUser}
+        darkMode={resolvedTheme === 'dark'}
+        menus={menus}
+        openMenu={openMenu}
+        changeTheme={changeTheme}
+        handleToggleMenu={handleToggleMenu}
+        preventPropagation={preventPropagation}
+      />
+
+      <AlertDialog
+        actions={
+          <div className="flex items-center justify-end gap-2">
+            <div className="w-fit">
+              <PrimaryButton type="button" onClick={handleToggleLogoutDialog}>
+                キャンセル
+              </PrimaryButton>
+            </div>
+            <div className="w-fit">
+              <PrimaryButton color="error" type="button" variant="contained" onClick={handleLogout}>
+                <ArrowLeftOnRectangleIcon className="h-5 w-5" />
+                ログアウト
+              </PrimaryButton>
+            </div>
+          </div>
+        }
+        open={openLogoutDialog}
+        text="ログアウトしますか？"
+        handleClose={handleToggleLogoutDialog}
+      />
+    </>
   )
 })
